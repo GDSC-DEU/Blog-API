b5e6b1e1487cf30b658917d9ff1546133cd52f2b
thelight0804 <thelight0804@gmail.com>
2023-06-29 23:46:42 +0900

    add: swagger description in user controller
    user controller API에 swagger 정보 추가

--- src/main/java/com/gdsc/blog/user/UserController.java
+++ src/main/java/com/gdsc/blog/user/UserController.java
@@ -1,6 +1,7 @@
 package com.gdsc.blog.user;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +17,8 @@ public class UserController {
     @ResponseBody
     @Operation(summary = "Login", description = "Login")
     public String login(
-        String id,
-        String password
+        @Parameter(name="id", description = "user id") String id,
+        @Parameter(name="password", description = "user password") String password
     ) {
         return "Hello, login!";
     }
