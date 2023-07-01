aac46be0d3071834e4e495b5d402546a79c21e0f
thelight0804 <thelight0804@gmail.com>
2023-07-02 00:04:49 +0900

    Move: test code from MemberSignupTest.java to BlogApplicationTests.java
    MemberSignupTest.java에 있던 테스트 코드를 기존의 BlogApplicationTests.java에 이동했습니다

--- src/test/java/com/gdsc/blog/BlogApplicationTests.java
+++ src/test/java/com/gdsc/blog/BlogApplicationTests.java
@@ -1,14 +1,34 @@
 package com.gdsc.blog;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.gdsc.blog.user.Member;
+import com.gdsc.blog.user.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 @Profile("test")
 class BlogApplicationTests {
 
-	@Test
-	void contextLoads() {}
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    @Test
+    public void testSignup() throws Exception {
+        String username = "test"; //set username
+        String password = "test123"; //set password
+
+        Member member = memberService.join(username, password); //sign up
+
+        assertEquals(username, member.getUsername());
+        //not use assertEquals to password
+        assertTrue(passwordEncoder.matches(password, member.getPassword()));
+    }
 }
