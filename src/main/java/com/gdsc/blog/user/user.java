9b151c585be5d917347027dc09186a2982bec8da
master <cj855695@gmail.com>
2023-07-03 03:53:52 +0900

    user 엔티티 클래스 생성

---
+++ src/main/java/com/gdsc/blog/user/user.java
@@ -0,0 +1,49 @@
+package com.gdsc.blog.user;
+
+public class user {
+    private String ID;
+    private String password;
+    private String address;
+    private String phone;
+    private String gender;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
