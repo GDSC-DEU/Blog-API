676b3837e4544c1628c3aad7921eef259b4393f5
thelight0804 <thelight0804@gmail.com>
2023-07-02 12:40:51 +0900

    Add: ignore spring security
    spring security 비활성화

--- src/main/java/com/gdsc/blog/SecurityConfig.java
+++ src/main/java/com/gdsc/blog/SecurityConfig.java
@@ -2,13 +2,26 @@ package com.gdsc.blog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests((authorize) -> authorize
+            .anyRequest().permitAll());
+
+        return http.build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
