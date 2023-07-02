d279a02390889eb27bf67fb1fc0079ea0fa6c1ab
thelight0804 <thelight0804@gmail.com>
2023-07-02 23:19:28 +0900

    Add: swagger operation to createJwtToken method

--- src/main/java/com/gdsc/blog/JwtToken.java
+++ src/main/java/com/gdsc/blog/JwtToken.java
@@ -3,6 +3,7 @@ package com.gdsc.blog;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.swagger.v3.oas.annotations.Operation;
 import java.time.Duration;
 import java.util.Date;
 
@@ -12,6 +13,7 @@ public class JwtToken {
      * create jwt token
      * @return jwt token
      */
+    @Operation(summary = "create jwt token")
     public String createJwtToken(){
         Date now = new Date(); //now date
 
