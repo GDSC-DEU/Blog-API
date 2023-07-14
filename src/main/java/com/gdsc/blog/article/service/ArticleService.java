ac9ac26c1a6c429c82517cee3c1ea87b5f99b725
thelight0804 <thelight0804@gmail.com>
2023-07-14 12:15:13 +0900

    Remove Swagger documentation in service
    사용자에게 노출되지 않는 Service에 Swagger documention 제거

--- src/main/java/com/gdsc/blog/article/service/ArticleService.java
+++ src/main/java/com/gdsc/blog/article/service/ArticleService.java
@@ -4,9 +4,6 @@ import com.gdsc.blog.article.dto.ArticleUpdateDto;
 import com.gdsc.blog.article.entity.Article;
 import com.gdsc.blog.article.repository.ArticleRepository;
 import com.gdsc.blog.user.entity.User;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -15,19 +12,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Tag(name = "게시글 Service", description = "Article service API")
 public class ArticleService {
+
     private final ArticleRepository articleRepository;
 
     /**
      * 게시글 생성
+     *
      * @param article Article 객체
-     * @param user 작성자
+     * @param user    작성자
      */
-    @Operation(summary = "게시글 생성")
     public Article createArticle(
-        @Parameter(name = "게시글 객체") Article article,
-        @Parameter(name = "유저 객체") User user){ //create new article
+        Article article,
+        User user) { //create new article
         article.setCreateDate(LocalDateTime.now());
         article.setModifyDate(LocalDateTime.now());
         article.setUser(user);
@@ -36,43 +33,38 @@ public class ArticleService {
 
     /**
      * 유저 게시글 목록 조회
+     *
      * @return 게시글 목록
      */
-    @Operation(summary = "유저 게시글 목록 가져오기")
-    public List<Article> getUserArticle(User user){ //get all articles
+    public List<Article> getUserArticle(User user) { //get all articles
         return user.getArticles();
     }
 
     /**
      * id로 게시글 조회
+     *
      * @param idx 게시글 id
      * @return Article 객체
      */
-    @Operation(summary = "게시글 조회")
-    public Article getArticleById(
-        @Parameter(name = "게시글 id") Long idx){ //get article by id
-        Optional <Article> article = articleRepository.findById(idx);
+    public Article getArticleById(Long idx) { //get article by id
+        Optional<Article> article = articleRepository.findById(idx);
 
-        if(article.isPresent()){
+        if (article.isPresent()) {
             return article.get();
-        }
-        else{ //not found article by id
+        } else { //not found article by id
             throw new NullPointerException("Not found Article by id");
         }
     }
 
     /**
      * 게시글 수정
-     * @param id 게시글 id
-     * @param dto 게시글 수정 DTO
+     *
+     * @param id   게시글 id
+     * @param dto  게시글 수정 DTO
      * @param user 작성자
      * @return Article 객체
      */
-    @Operation(summary = "게시글 수정")
-    public Article updateArticle(
-        @Parameter(name = "게시글 id") Long id,
-        @Parameter(name = "게시글 수정 DTO") ArticleUpdateDto dto,
-        @Parameter(name = "작성자") User user){
+    public Article updateArticle(Long id, ArticleUpdateDto dto, User user) {
 
         Article article = getArticleById(id); //게시글 id로 게시글 가져오기
         article.setTitle(dto.getTitle());
@@ -83,11 +75,10 @@ public class ArticleService {
 
     /**
      * 게시글 삭제
+     *
      * @param article 게시글 객체
      */
-    @Operation(summary = "게시글 삭제")
-    public void deleteArticle(
-        @Parameter(name = "개시글 객체") Article article){
+    public void deleteArticle(Article article) {
         articleRepository.delete(article);
     }
 }
--- src/main/java/com/gdsc/blog/comment/service/CommentService.java
+++ src/main/java/com/gdsc/blog/comment/service/CommentService.java
@@ -5,9 +5,6 @@ import com.gdsc.blog.comment.dto.CommentUpdateDto;
 import com.gdsc.blog.comment.entity.Comment;
 import com.gdsc.blog.comment.repository.CommentRepository;
 import com.gdsc.blog.user.entity.User;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -16,60 +13,53 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Tag(name = "댓글 service", description = "Comment service API")
 public class CommentService {
+
     private final CommentRepository commentRepository;
 
     /**
      * 댓글 생성
+     *
      * @param comment 댓글 객체
      * @param article 개시글 객체
-     * @param user 로그인 유저 객체
+     * @param user    로그인 유저 객체
      */
-    @Operation(summary = "댓글 생성")
-    public Comment create(
-        @Parameter(name = "댓글 객체") Comment comment,
-        @Parameter(name = "게시글 내용") Article article,
-        @Parameter(name = "로그인 유저") User user){
+    public Comment create(Comment comment, Article article, User user) {
         comment.setArticle(article);
         comment.setUser(user);
         comment.setCreateData(LocalDateTime.now());
         comment.setModifyData(LocalDateTime.now());
-        
+
         return commentRepository.save(comment);
     }
 
-    public List<Comment> getAllCommandByArticle(Article article){
+    public List<Comment> getAllCommandByArticle(Article article) {
         return commentRepository.findByArticle(article);
     }
 
     /**
      * id로 댓글 조회
+     *
      * @param idx 댓글 id
      * @return 댓글 객체
      */
-    @Operation(summary = "id로 댓글 조회")
-    public Comment getCommentById(
-        @Parameter(name = "댓글 id") Long idx) {
-        Optional <Comment> comment = commentRepository.findById(idx);
+    public Comment getCommentById(Long idx) {
+        Optional<Comment> comment = commentRepository.findById(idx);
 
-        if(comment.isPresent()){
+        if (comment.isPresent()) {
             return comment.get();
-        }
-        else{
+        } else {
             throw new NullPointerException("Not found Comment by id");
         }
     }
 
     /**
      * 댓글 수정
+     *
      * @param id 댓글 id
      * @param commentUpdateDto 댓글 수정 DTO
      */
-    @Operation(summary = "댓글 수정")
-    public Comment updateComment(
-        @Parameter(name = "댓글 id") Long id,
-        @Parameter(name = "댓글 수정 DTO") CommentUpdateDto commentUpdateDto){
+    public Comment updateComment(Long id, CommentUpdateDto commentUpdateDto) {
 
         Comment comment = getCommentById(id);
         comment.setContent(commentUpdateDto.getContent());
@@ -81,11 +71,10 @@ public class CommentService {
 
     /**
      * 댓글 삭제
+     *
      * @param comment 댓글 객체
      */
-    @Operation(summary = "댓글 삭제")
-    public void delete(
-        @Parameter(name = "댓글", description = "댓글 객체") Comment comment){
+    public void delete(Comment comment) {
         commentRepository.delete(comment);
     }
 }
